99714efb6f9643555ab8060dcfbdfc63da58d213
pham_trung_dung <21020176@vnu.edu.vn>
2022-12-11 22:56:35 +0700

    update

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import "./navbar.css"
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navContainer">
+        <a href="profile" className="logo">HoteLoka</a>
+        <div className="navItems">
+          <h1>Welcome back!</h1>
+          
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
